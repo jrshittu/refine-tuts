677339dfd2457437cca2016282a84a5ed88d8c1d
Abideen <jrshittu@gmail.com>
2023-10-19 18:32:16 +0100

    refine tuts

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
 import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
-
-import { notificationProvider, RefineSnackbarProvider } from "@refinedev/mui";
-
+import { 
+  notificationProvider, 
+  ThemedLayoutV2,
+  ErrorComponent,
+  RefineThemes,
+  RefineSnackbarProvider 
+} from "@refinedev/mui";
 import CssBaseline from "@mui/material/CssBaseline";
 import GlobalStyles from "@mui/material/GlobalStyles";
+import { ThemeProvider } from "@mui/material";
 import routerBindings, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import dataProvider from "@refinedev/simple-rest";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
 import { ColorModeContextProvider } from "./contexts/color-mode";
+import { MuiInferencer } from '@refinedev/inferencer/mui'
 
 function App() {
   return (
-    <BrowserRouter>
+    <ThemeProvider theme={RefineThemes.Blue}>
+      <BrowserRouter>
       <GitHubBanner />
       <RefineKbarProvider>
         <ColorModeContextProvider>
@@ -28,6 +35,15 @@ function App() {
                 notificationProvider={notificationProvider}
                 routerProvider={routerBindings}
                 dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+                resources={[
+                  {
+                      name: "blog_posts",
+                      list: "/blog-posts",
+                      show: "/blog-posts/show/:id",
+                      create: "/blog-posts/create",
+                      edit: "/blog-posts/edit/:id",
+                  },
+              ]}
                 options={{
                   syncWithLocation: true,
                   warnWhenUnsavedChanges: true,
@@ -35,7 +51,30 @@ function App() {
                 }}
               >
                 <Routes>
-                  <Route index element={<WelcomePage />} />
+                    <Route 
+                      element={
+                        <ThemedLayoutV2>
+                            <Outlet />
+                        </ThemedLayoutV2>}
+                    >
+                      <Route index element={<WelcomePage />} />
+                      <Route path="blog-posts">
+                                    <Route index element={<MuiInferencer />} />
+                                    <Route
+                                        path="show/:id"
+                                        element={<MuiInferencer />}
+                                    />
+                                    <Route
+                                        path="edit/:id"
+                                        element={<MuiInferencer />}
+                                    />
+                                    <Route
+                                        path="create"
+                                        element={<MuiInferencer />}
+                                    />
+                    </Route>
+                  <Route path="*" element={<ErrorComponent />} />
+                </Route>     
                 </Routes>
                 <RefineKbar />
                 <UnsavedChangesNotifier />
@@ -47,7 +86,9 @@ function App() {
         </ColorModeContextProvider>
       </RefineKbarProvider>
     </BrowserRouter>
-  );
+
+    </ThemeProvider>
+      );
 }
 
 export default App;
